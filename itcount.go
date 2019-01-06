788bc81105e2780b480d50ccc56686c9caa25528
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-01-06 18:29:46 -0200

    Got basic hello world working

--- itcount.go
+++ itcount.go
@@ -1,7 +1,17 @@
+//+build js,wasm
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
 	fmt.Println("webasm moderfocker")
+
+	document := js.Global().Get("document")
+	div := document.Call("getElementById", "target")
+	node := document.Call("createElement", "div")
+	div.Call("appendChild", node)
+	node.Set("innerText", "Hello World")
 }
