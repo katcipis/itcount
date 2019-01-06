953005f52061361b03db05ca5191ce10990a59f8
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-01-06 18:58:05 -0200

    Update HTML

--- itcount.go
+++ itcount.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+	"time"
 )
 
 func main() {
@@ -12,7 +13,29 @@ func main() {
 	document := js.Global().Get("document")
 	counterVal := document.Call("createElement", "div")
 
-	counter := document.Call("getElementById", "counter")
-	counter.Call("appendChild", counterVal)
-	counterVal.Set("innerText", "Hello World")
+	counterDiv := document.Call("getElementById", "counter")
+	counterDiv.Call("appendChild", counterVal)
+
+	for {
+		fmt.Println("start loop")
+		val := count()
+		fmt.Println(val)
+		counterVal.Set("innerText", val)
+		time.Sleep(time.Second)
+	}
+}
+
+func count() string {
+	year := 2019
+	month := time.January
+	day := 30
+	hour := 0
+	min := 0
+	sec := 0
+	nsec := 0
+	location := time.UTC
+
+	deadline := time.Date(year, month, day, hour, min, sec, nsec, location)
+
+	return time.Until(deadline).String()
 }
