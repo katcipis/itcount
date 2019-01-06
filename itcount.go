a6dff5e43a2a55bb13b7f2efaf8cc442f7dffdd7
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-01-06 18:32:33 -0200

    rename some stuff

--- itcount.go
+++ itcount.go
@@ -10,8 +10,9 @@ func main() {
 	fmt.Println("webasm moderfocker")
 
 	document := js.Global().Get("document")
-	div := document.Call("getElementById", "target")
-	node := document.Call("createElement", "div")
-	div.Call("appendChild", node)
-	node.Set("innerText", "Hello World")
+	counterVal := document.Call("createElement", "div")
+
+	counter := document.Call("getElementById", "counter")
+	counter.Call("appendChild", counterVal)
+	counterVal.Set("innerText", "Hello World")
 }
